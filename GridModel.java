49ebde02cf303b20cea3bb2779c047339e56d3df
ManiKumar.Korthivada <mkorthivada@deloitte.com>
2019-05-10 19:54:34 +0530

    Grid Package

--- GridModel.java
+++ GridModel.java
@@ -1,33 +1,59 @@
 package com.aetna.ahm.core.models.qr;
 
+import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.sightly.WCMUsePojo;
 import com.google.common.collect.Iterators;
+import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import java.util.Iterator;
 
 
 /**
  * The Class ConfigDataUse.
  */
-public class GridModel extends WCMUsePojo {
+@Model(adaptables = {SlingHttpServletRequest.class,Resource.class})
+public class GridModel {
 
+
+    @Inject
+    SlingHttpServletRequest request;
 
     int noOfComponents = 0;
+
+    boolean isVideoComponent = false;
 
     /*
      * (non-Javadoc)
      *
      * @see com.adobe.cq.sightly.WCMUsePojo#activate()
      */
-    @Override
-    public void activate() throws Exception {
-        Resource resource = getResource();
+    @PostConstruct
+    public void init() throws Exception {
+        Resource resource = request.getResource();
         Iterator<Resource> iterator = resource.listChildren();
+        while (iterator.hasNext()){
+            Resource componentResource = iterator.next();
+            ValueMap valueMap = componentResource.getValueMap();
+            String resourceType = valueMap.get("sling:resourceType",String.class);
+            if("ahm/components/content/video".equalsIgnoreCase(resourceType)){
+                isVideoComponent = true;
+            }
+        }
+        iterator = resource.listChildren();
         noOfComponents = Iterators.size(iterator);
     }
 
     public int getNoOfComponents() {
         return noOfComponents;
+    }
+
+    public boolean isVideoComponent() {
+        return isVideoComponent;
     }
 }
--- GridModelTest.java
+++ GridModelTest.java
@@ -2,6 +2,8 @@ package com.aetna.ahm.core.models.qr;
 
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
@@ -29,7 +31,15 @@ public class GridModelTest {
         Resource resource =  Mockito.mock(Resource.class);
         Mockito.when(request.getResource()).thenReturn(resource);
         Iterator<Resource> iterator = Mockito.mock(Iterator.class);
+        Resource componentResource = Mockito.mock(Resource.class);
+        Mockito.when(iterator.hasNext()).thenReturn(true).thenReturn(false);
+        Mockito.when(iterator.next()).thenReturn(componentResource);
+        ValueMap valueMap = Mockito.mock(ValueMap.class);
+        Mockito.when(componentResource.getValueMap()).thenReturn(valueMap);
+        Mockito.when(valueMap.get("sling:resourceType",String.class)).thenReturn("ahm/components/content/video");
         Mockito.when(resource.listChildren()).thenReturn(iterator);
-        gridModel.getNoOfComponents();
+        gridModel.init();
+        Assert.assertEquals(0,gridModel.getNoOfComponents());
+        Assert.assertEquals(true,gridModel.isVideoComponent());
     }
 }
