e3b3fa491fe3cbb1d6d74901f97e0da101922f77
ManiKumar.Korthivada <mkorthivada@deloitte.com>
2019-10-30 23:21:52 +0530

    Update GridModelTest.java

--- GridModelTest.java
+++ GridModelTest.java
@@ -1,45 +1,48 @@
-package com.aetna.ahm.core.models.qr;
+package ahm.content.service.core.models;
 
+import com.day.cq.tagging.Tag;
+import com.day.cq.tagging.TagManager;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
-import org.apache.sling.api.resource.ValueMap;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.powermock.api.support.membermodification.MemberModifier;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.Iterator;
 
-
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(GridModel.class)
+@PrepareForTest({GridModel.class})
 public class GridModelTest {
 
-
     @InjectMocks
-    GridModel gridModel;
+    private GridModel gridModel;
 
-    @Mock
     SlingHttpServletRequest request;
+
+    @Before
+    public void setup() throws IllegalArgumentException, IllegalAccessException {
+        request = Mockito.mock(SlingHttpServletRequest.class);
+        MemberModifier.field(GridModel.class,"request").set(gridModel,request);
+    }
 
     @Test
     public void test() throws Exception{
-        Resource resource =  Mockito.mock(Resource.class);
+        Resource resource = Mockito.mock(Resource.class);
         Mockito.when(request.getResource()).thenReturn(resource);
+        Mockito.when(resource.getResourceType()).thenReturn("dgtl-content/components/structure/xfpagewidget");
+        Mockito.when(resource.getChild("root")).thenReturn(resource);
         Iterator<Resource> iterator = Mockito.mock(Iterator.class);
-        Resource componentResource = Mockito.mock(Resource.class);
-        Mockito.when(iterator.hasNext()).thenReturn(true).thenReturn(false);
-        Mockito.when(iterator.next()).thenReturn(componentResource);
-        ValueMap valueMap = Mockito.mock(ValueMap.class);
-        Mockito.when(componentResource.getValueMap()).thenReturn(valueMap);
-        Mockito.when(valueMap.get("sling:resourceType",String.class)).thenReturn("ahm/components/content/video");
         Mockito.when(resource.listChildren()).thenReturn(iterator);
+        Mockito.when(iterator.hasNext()).thenReturn(true).thenReturn(false);
         gridModel.init();
-        Assert.assertEquals(0,gridModel.getNoOfComponents());
-        Assert.assertEquals(true,gridModel.isVideoComponent());
+        Assert.assertEquals(1,gridModel.getNoOfComponents());
     }
 }
